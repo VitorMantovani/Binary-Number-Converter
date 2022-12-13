779011d65ec3d131de45b62f3b67367676fab30b
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-13 16:12:24 -0300

    feat: convertSimpleBinary() implemented

--- src/BinaryToDecimal.ts
+++ src/BinaryToDecimal.ts
@@ -1,3 +1,14 @@
 export class BinaryToDecimal {
-    
+    constructor(private input: string) {}
+    convertSimpleBinary(): number {
+        const reversedBinaryChars: number[] = this.input.split("").map(Number).reverse();
+
+        const decimalNumber: number = reversedBinaryChars.reduce((acc, cur, idx) => {
+            console.log(idx)
+            acc += cur * (Math.pow(2, idx)) 
+            return acc
+        }, 0)
+
+        return decimalNumber
+    }
 }
