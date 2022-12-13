7847eab6d194ead679d9d823ccfaf97aa099976e
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-13 16:35:51 -0300

    feat: convertBinaryWithComma() implemented

--- src/BinaryToDecimal.ts
+++ src/BinaryToDecimal.ts
@@ -1,14 +1,32 @@
 export class BinaryToDecimal {
     constructor(private input: string) {}
+
     convertSimpleBinary(): number {
         const reversedBinaryChars: number[] = this.input.split("").map(Number).reverse();
 
         const decimalNumber: number = reversedBinaryChars.reduce((acc, cur, idx) => {
-            console.log(idx)
             acc += cur * (Math.pow(2, idx)) 
             return acc
         }, 0)
 
         return decimalNumber
+    }
+
+    convertBinaryWithComma(): number {
+        const separetedChars: string[] = this.input.split("");
+
+        const charsAfterComma: string[] = separetedChars.slice(separetedChars.indexOf(",") + 1)
+
+        const binaryChars: number[] = this.input.replace(",", "").split("").map(Number);
+
+        let index = -charsAfterComma.length
+
+        const decimalNumber: number = binaryChars.reduceRight((acc, curr) => {
+            acc += curr * Math.pow(2, index);
+            index++
+            return acc;
+        }, 0)
+
+        return decimalNumber;
     }
 }
