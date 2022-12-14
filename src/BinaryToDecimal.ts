42b9206a29a6c4641e900d6f61881100113ddace
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-14 14:10:46 -0300

    refactor: convertBinary() refactored

--- src/BinaryToDecimal.ts
+++ src/BinaryToDecimal.ts
@@ -1,32 +1,12 @@
 export class BinaryToDecimal {
-    constructor(private input: string) {}
+  constructor(private array: number[], private index: number) {}
 
-    convertSimpleBinary(): number {
-        const reversedBinaryChars: number[] = this.input.split("").map(Number).reverse();
-
-        const decimalNumber: number = reversedBinaryChars.reduce((acc, cur, idx) => {
-            acc += cur * (Math.pow(2, idx)) 
-            return acc
-        }, 0)
-
-        return decimalNumber
-    }
-
-    convertBinaryWithComma(): number {
-        const separetedChars: string[] = this.input.split("");
-
-        const charsAfterComma: string[] = separetedChars.slice(separetedChars.indexOf(",") + 1)
-
-        const binaryChars: number[] = this.input.replace(",", "").split("").map(Number);
-
-        let index = -charsAfterComma.length
-
-        const decimalNumber: number = binaryChars.reduceRight((acc, curr) => {
-            acc += curr * Math.pow(2, index);
-            index++
-            return acc;
-        }, 0)
-
-        return decimalNumber;
-    }
+  convertBinary(): number {
+    const decimalNumber = this.array.reduceRight((decimal, algo) => {
+        decimal += algo * Math.pow(2, this.index)
+        this.index++
+        return decimal;
+    }, 0)
+    return decimalNumber;
+  }
 }
