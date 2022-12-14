1f39b014e960cdb8b7c70f195d80a920dc2ea0ed
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-14 13:50:34 -0300

    feat: splitInput() implemented

--- src/InputToArray.ts
+++ src/InputToArray.ts
@@ -1,5 +1,9 @@
 export class InputToArray {
-    constructor(private input: string) {}
+  constructor(private input: string) {}
 
-
+  splitInput(): number[] {
+    if (this.input.includes(",")) this.input = this.input.replace(",", "");
+    const arrayOfChars = this.input.split("").map(Number)
+    return arrayOfChars;
+  }
 }
