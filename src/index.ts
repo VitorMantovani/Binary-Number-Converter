16bb05cd6e91694fdfe29b52c7337c803ab3fd45
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-14 14:19:33 -0300

    refactor: index.ts refactored

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-function binaryToDecimal(binary: string): number {
-    
+import { BinaryToDecimal } from "./BinaryToDecimal"
+import { IndexCatcher } from "./IndexCatcher"
+import { InputToArray } from "./InputToArray"
+import { ValidateInput } from "./ValidateInput"
+
+const BINARY = "1010,10"
+
+const validateInput = new ValidateInput(BINARY);
+
+const indexCatcher = new IndexCatcher(BINARY);
+
+const inputToArray = new InputToArray(BINARY)
+
+if(!validateInput.isInputEmpty()) {
+    if(validateInput.isBinary()) {
+        const inicialIndex = indexCatcher.catchInicialIndex()
+        const arrayOfChars = inputToArray.splitInput();
+        const binaryToDecimal = new BinaryToDecimal(arrayOfChars, inicialIndex);
+        const decimal = binaryToDecimal.convertBinary();
+        console.log(decimal);
+    }
 }
+
+
+
+
